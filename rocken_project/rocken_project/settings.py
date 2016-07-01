876e40a7f58ecaf566d2f0994748f1db40bff056
payalpandit123 <panditpayal11@gmail.com>
2016-07-01 16:46:55 +0530

    added app into installed app in setting.py

--- rocken_project/rocken_project/settings.py
+++ rocken_project/rocken_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Invoice',
 ]
 
 MIDDLEWARE = [
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'rocken_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR,  '../Project_Database/db.sqlite3'),
     }
 }
 
